5dcf8887097562bdb21f95dd55d27a22adcd42d2
Jeon Ga Bin <hyanghope@naver.com>
2019-03-26 00:30:13 +0900

    Nesting

---
+++ 7- Nesting/Nesting.cpp
@@ -0,0 +1,32 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+#include <stack>
+stack<char> s;
+int solution(string &S) {
+    // write your code in C++14 (g++ 6.2.0)
+    int len = S.size();
+    for(int i=0;i<len;i++){
+        if(S[i]=='('){
+          s.push(S[i]);  
+        }
+        else{
+            if(s.empty())
+                return 0;
+            char temp = s.top();
+            
+            if(temp=='('){
+                if(')'!=S[i])
+                    return 0;
+            }
+           
+            s.pop();
+        }
+    }
+    if(!s.empty())
+        return 0;
+    return 1;
+}
